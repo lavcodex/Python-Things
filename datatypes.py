81f6dcebebcf5dadbb49480f7d61f7b028acd980
Lavanya <lavanyashivaraju2@gmail.com>
2025-04-02 20:30:16 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+a = complex(8,2)
+b = True
+c = "lavu"
+d = None
+print(a)
+print(b)
+a1 = 9
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
