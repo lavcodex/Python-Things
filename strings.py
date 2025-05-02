ac59299f372e46e3af38cbcbf6d78477feefd05d
Lavanya <lavanyashivaraju2@gmail.com>
2025-05-02 13:59:50 +0530

    Python Refresher

---
+++ strings.py
@@ -0,0 +1,21 @@
+name1 = "Lavuu"
+print(name1)
+
+print(name1.upper())
+
+print(name1.endswith("ry"))
+
+print(name1.endswith("jerry"))
+
+print(name1.lower())
+
+print(name1.count("a"))
+
+print(name1.count("u"))
+
+s = "LavanyaShivaraju"
+print(len(s))
+
+print(s[1:12])
+print(s[-21:-12])
+print(s[0:5])
