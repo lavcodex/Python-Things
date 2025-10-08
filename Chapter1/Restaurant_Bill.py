6060d822cc5dbf1f0dac23e9e5cbac747f466b76
lavcodex <lavanyashivaraju2@gmail.com>
2025-10-08 21:01:44 +0530

    Added code file

---
+++ Chapter1/Restaurant_Bill.py
@@ -0,0 +1,4 @@
+#Calculate variables needed for a restaurant bill.
+amount= float(input("Enter a bill amount:"))
+number=int(input("Enter a table number:"))
+print(f"Restaurant Bill \nBill amount(before tip) {amount} and table number {number}")
