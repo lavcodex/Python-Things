72646e326a54469dbede8ab4e50c22af9c9ce6f6
lavcodex <lavanyashivaraju2@gmail.com>
2025-10-08 17:54:13 +0530

    some changes

--- Chapter1/Online_Store_Login.py
+++ Chapter1/Online_Store_Login.py
@@ -1,4 +1,5 @@
 #Track whether a user is logged into an online store.
+
 name=input("Enter a username:")
 status_input=input("Enter a login status(True/False):").lower()
 status = True if status_input == "True" else False
