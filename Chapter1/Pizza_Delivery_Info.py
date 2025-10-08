ab4a5cd33f7408d50e6140efdddba13a913aa406
lavcodex <lavanyashivaraju2@gmail.com>
2025-10-08 19:59:07 +0530

    Added code file

---
+++ Chapter1/Pizza_Delivery_Info.py
@@ -0,0 +1,5 @@
+# A pizza place needs to store delivery information.
+
+address=str(input("Enter a delivery address:"))
+count=int(input("Enter a pizza count:"))
+print(f"pizza delivery info \nThe customer address {address} and {count} pizzas.")
---
+++ Chapter1/Student_Grade_Tracker.py
@@ -0,0 +1,4 @@
+# A teacher needs to record a student's test score.
+name=str(input("Enter a student name:"))
+score=float(input("Enter a percentage score:"))
+print(f"Student Grade Tracker \nThe {name} as scored {score} as the percentage")
