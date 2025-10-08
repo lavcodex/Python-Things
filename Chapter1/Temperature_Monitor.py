2e2e22eb46d40bc519e3bbe19d2f8218a41be54d
lavcodex <lavanyashivaraju2@gmail.com>
2025-10-08 17:34:37 +0530

    Added code file

---
+++ Chapter1/Temperature_Monitor.py
@@ -0,0 +1,4 @@
+# A weather station needs to record today's temperature.
+temp=float(input("Enter a temperature:"))
+loc=input("Enter a location:")
+print(f" Temperature Monitor \nThe weather at {loc} today is {temp} degrees.")
