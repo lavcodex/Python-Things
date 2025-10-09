b3b3ca3724b56118408b3d3c32971b2e6a53bd24
lavcodex <lavanyashivaraju2@gmail.com>
2025-10-09 06:30:27 +0530

    Added code file

---
+++ Chapter1/Movie_Theater_Seats.py
@@ -0,0 +1,4 @@
+#A theater needs to track available seats for a show.
+title=str(input("Enter a movie title:"))
+seats=int(input("Enter an available seats:"))
+print(f"Movie Theater Seats \nmovie title {title} and it's {seats} seats available")
---
+++ Chapter1/Parking_Garage_System.py
@@ -0,0 +1,8 @@
+#Track if parking spaces are occupied.
+num=int(input("Enter a parking space number:"))
+occupied_status= False
+if occupied_status:
+    print(f"parking Space {num} and is occupied space")
+else:
+    print(f"parking Space {num} and is empty space")
+
