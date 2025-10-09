7a05aff19ab2aa8fa5a0836a0015b71ca1ef1cd6
lavcodex <lavanyashivaraju2@gmail.com>
2025-10-09 06:35:42 +0530

    some changes

--- Chapter1/Bank_Account_Creation.py
+++ Chapter1/Bank_Account_Creation.py
@@ -1,4 +1,4 @@
 #A bank creates a new account for a customer
 name=str(input("Enter a account holder name:"))
-deposit=float(input("Enter a deposit ammount:"))
-print(f"Bank Account Creation \nAccount Holder is {name} and your initial deposit {deposit} ")
+amount=float(input("Enter a deposit ammount:"))
+print(f"Bank Account Creation \nAccount Holder is {name} and your initial deposit {amount} ")
