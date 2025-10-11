dc09ee0e9b129806eaedf6ea8b2c8b26af63954a
lavcodex <lavanyashivaraju2@gmail.com>
2025-10-11 18:40:03 +0530

    Added code file

---
+++ Chapter1/Smart_Home_Energy_Monitor.py
@@ -0,0 +1,20 @@
+# A smart home system tracks appliance usage and converts between different data formats.
+name = "Air Conditioner"
+consumption = "1.5"
+hours = "8.5"
+rate = "0.12"
+status = "on"
+
+conv_consumption = float(consumption)
+conv_hours = float(hours)
+conv_rate = float(rate)
+
+total = conv_consumption * conv_hours
+cost = total * conv_rate
+if status:
+    print("Device Status is True")
+else:
+    print("Device Status is False")
+result = round(cost,2)
+print(f"Total cost: {result}")
+print(f"Status tracking \ndevice name: {name}, consumption: {conv_consumption}, cost: {cost}, status: {status}")
