7e61074561bdedce847477a99573f25a021ac7b8
lavcodex <lavanyashivaraju2@gmail.com>
2025-10-12 22:04:28 +0530

    Added code file

---
+++ Chapter1/Healthcare_Patient_Records.py
@@ -0,0 +1,9 @@
+# Medical system processing patient data with critical accuracy requirements.
+ID = "PAT2024-0892"
+age = "45"
+weight = "168.5"
+height = "70"
+blood = "120/80"
+critical = "false"
+extract_year = 2024
+
