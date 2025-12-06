d0b04196967c75bd648222e1d03f13053f38f8d3
lavcodex <lavanyashivaraju2@gmail.com>
2025-12-06 18:40:29 +0530

    some changes

--- Chapter2/Password_Attempt_Limiter.py
+++ Chapter2/Password_Attempt_Limiter.py
@@ -1,7 +1,17 @@
 # Allow user three attempts to enter correct password.
 
-correct_password="lavu123"
-attempt=1
+correct_password = "lavu123"
+attempt = 1
 
-while attempt<=3:
-    password=int
+while attempt <= 3:
+    password = input("Enter password: ")
+
+    if password == correct_password:
+        print("Access granted")
+        break
+    else:
+        print("Incorrect password")
+        attempt += 1
+
+if attempt > 3:
+    print("Account Locked")
