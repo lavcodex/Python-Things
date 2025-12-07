0ec0f9560c2e3b7df1001b8090910b99a1b922c6
lavcodex <lavanyashivaraju2@gmail.com>
2025-12-07 17:50:26 +0530

    Added code file

---
+++ Chapter2/Grade_Letter_Assignment.py
@@ -0,0 +1,16 @@
+# Convert numeric score to letter grade.
+
+score = int(input("enter score:"))
+
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'F'
+    
+print("grade:",grade)
