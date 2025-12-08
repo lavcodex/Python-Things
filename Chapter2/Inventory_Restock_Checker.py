304d8af7676d8242f3bc7e3eb2cbc20bd6e1928f
lavcodex <lavanyashivaraju2@gmail.com>
2025-12-08 19:09:12 +0530

    Added code file

---
+++ Chapter2/Inventory_Restock_Checker.py
@@ -0,0 +1,13 @@
+# Check each product in store to see if it needs restocking.
+
+products = ["soap","shampoo","oil","sugar","rice"]
+current_stock = [10,15,16,20,5]
+minimum_stock = [5,9,20,15,13]
+restock_list = []
+
+for i in range(len(products)):
+    if current_stock[i]<minimum_stock[i]:
+        restock_list.append(products[i])
+        
+print("products that needs restocking:")
+print(restock_list)
